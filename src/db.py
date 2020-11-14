016bb8f62b046af0519cf2ff2693fb570b0d29d7
matthewvday <matthewvday@gmail.com>
2020-11-14 14:44:47 -0500

    added methods to edit elements. Need to know params, will update after

--- src/db.py
+++ src/db.py
@@ -64,8 +64,40 @@ def get_password(email):
         return {"found": True, "pass": res[0][0]}
 
 
+def add_element(element):
+    cursor = my_db.cursor()
+    cmd = "INSERT INTO Tbl_Elements (Elements_Name) VALUES " \
+          "(%s)"
+
+    vls = (element,)
+    cursor.execute(cmd, vls)
+    my_db.commit()
+    cursor.close()
+
+
+def delete_element(element):
+    cursor = my_db.cursor()
+    cmd = "DELETE FROM Tbl_Elements WHERE " \
+          "Elements_Name= (%s)"
+
+    vls = (element,)
+    cursor.execute(cmd, vls)
+    my_db.commit()
+    cursor.close()
+
+
+def edit_element(new, element):
+    cursor = my_db.cursor()
+    cmd = "UPDATE Tbl_Elements SET Elements_Name= (%s) WHERE " \
+          "Elements_Name= (%s)"
+
+    vls = (new, element)
+    cursor.execute(cmd, vls)
+    my_db.commit()
+    cursor.close()
+
+
 if __name__ == '__main__':
-    # username = "guy"
-    # check_if_username_exists(username)
-    email = "test@gmail.com"
-    print(get_password(email))
+    thing = "trail mix"
+    thing2 = "gum"
+    edit_element(thing2, thing)
