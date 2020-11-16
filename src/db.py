aef030618b37c6fb819abb1d87f1b454e7e538fb
matthewvday <matthewvday@gmail.com>
2020-11-16 15:02:53 -0500

    reformatted methods to work with db pool

--- src/db.py
+++ src/db.py
@@ -3,7 +3,7 @@ import mysql.connector.pooling
 import os
 
 
-my_db = mysql.connector.connect(
+cnxpool = mysql.connector.pooling.MySQLConnectionPool(
     pool_name="mypool",
     pool_size=3,
     host=os.getenv("DB_HOST"),
@@ -14,6 +14,7 @@ my_db = mysql.connector.connect(
 
 
 def sign_up_db(first_name, last_name, email, password):
+    my_db = cnxpool.get_connection()
     cursor = my_db.cursor()
     cmd = "INSERT INTO Tbl_Users (Users_Email, Users_Verified, Users_Password, " \
           "Users_First_Name, Users_Last_Name, Users_Username) VALUES " \
@@ -23,14 +24,18 @@ def sign_up_db(first_name, last_name, email, password):
     cursor.execute(cmd, vls)
     my_db.commit()
     cursor.close()
+    my_db.close()
 
 
 def check_if_user_exists_by_email(email):
+    my_db = cnxpool.get_connection()
     cursor = my_db.cursor()
     sql = "SELECT COUNT(1) FROM Tbl_Users WHERE Users_Email = %s"
     cursor.execute(sql, (email,))
 
     res = cursor.fetchall()
+    cursor.close()
+    my_db.close()
 
     if res[0][0] == 0:
         print("Email does not exist")
@@ -41,11 +46,14 @@ def check_if_user_exists_by_email(email):
 
 
 def check_if_username_exists(username):
+    my_db = cnxpool.get_connection()
     cursor = my_db.cursor()
     sql = "SELECT COUNT(1) FROM Tbl_Users WHERE Users_Username = %s"
     cursor.execute(sql, (username,))
 
     res = cursor.fetchall()
+    cursor.close()
+    my_db.close()
 
     if res[0][0] == 0:
         print("Username does not exist")
@@ -56,11 +64,14 @@ def check_if_username_exists(username):
 
 
 def get_password(email):
+    my_db = cnxpool.get_connection()
     cursor = my_db.cursor()
     sql = "SELECT Users_Password FROM Tbl_Users WHERE Users_Email = %s"
     cursor.execute(sql, (email,))
 
     res = cursor.fetchall()
+    cursor.close()
+    my_db.close()
 
     if len(res) == 0:
         return {"found": False, "pass": ""}
@@ -69,6 +80,7 @@ def get_password(email):
 
 
 def add_element(element):
+    my_db = cnxpool.get_connection()
     cursor = my_db.cursor()
     cmd = "INSERT INTO Tbl_Elements (Elements_Name) VALUES " \
           "(%s)"
@@ -77,9 +89,11 @@ def add_element(element):
     cursor.execute(cmd, vls)
     my_db.commit()
     cursor.close()
+    my_db.close()
 
 
 def delete_element(element):
+    my_db = cnxpool.get_connection()
     cursor = my_db.cursor()
     cmd = "DELETE FROM Tbl_Elements WHERE " \
           "Elements_Name= (%s)"
@@ -88,9 +102,11 @@ def delete_element(element):
     cursor.execute(cmd, vls)
     my_db.commit()
     cursor.close()
+    my_db.close()
 
 
 def edit_element(new, element):
+    my_db = cnxpool.get_connection()
     cursor = my_db.cursor()
     cmd = "UPDATE Tbl_Elements SET Elements_Name= (%s) WHERE " \
           "Elements_Name= (%s)"
@@ -99,9 +115,9 @@ def edit_element(new, element):
     cursor.execute(cmd, vls)
     my_db.commit()
     cursor.close()
+    my_db.close()
 
 
 if __name__ == '__main__':
-    thing = "trail mix"
-    thing2 = "gum"
-    edit_element(thing, thing2)
+    email = "seb100@gmail.com"
+    check_if_user_exists_by_email(email)
